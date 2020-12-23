57304ad8529b2aab0a209cbe249f8abd2853dcaf
EmirAyva <76449501+EmirAyva@users.noreply.github.com>
2020-12-23 11:19:19 -0600

    Add files via upload

---
+++ Emir Ayva 1. Gun Odev.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+print("1,2,3,4,5")
+
+
+# In[8]:
+
+
+list= ("1,2,3,4,5")
+type(list)
+
+
+# In[10]:
+
+
+firstvalue= 15
+secondvalue= 16
+
+print("My value:{} and Your value:{}".format(firstvalue,secondvalue))
+
+
+# In[11]:
+
+
+firstvalue= 15
+secondvalue= 16
+
+print(f"My value{firstvalue}",f"Your value{secondvalue}")
+
+
+# In[ ]:
+
+
+
+
